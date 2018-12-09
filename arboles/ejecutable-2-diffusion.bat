f6b3e9ebe4214f1c6b21b9972c017a3102531ba1
Tonatihu <carlostonatihu@gmail.com>
2018-12-09 17:24:00 -0600

    materia terminada

--- arboles/ejecutable-2-diffusion.bat
+++
@@ -1,3 +0,0 @@
-set MATHE_PATH=C:\Program Files\Wolfram Research\Mathematica\11.3
-set PROJECT_PATH=C:\Users\reymy\Documents\septimo\computing-selected-topics\arboles
-"%MATHE_PATH%\wolframscript.exe" -cloud -print -format PNG -file "%PROJECT_PATH%\tam-2-diffusion.wls" > tam-2-diffusion.png
